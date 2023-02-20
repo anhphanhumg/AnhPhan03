b9de36673a9cc5083ad06201ab7556c6e2ac7b39
anhphanhumg <anhphanck@gmail.com>
2023-02-20 17:13:15 +0700

    Create Bai_01.java

---
+++ Bai_01.java
@@ -0,0 +1,12 @@
+package Bai_01;
+
+public class Bai_01 {
+	public static void main(String[] args) {
+		System.out.println("Phan Tuan Anh hoc Java");
+	}
+
+	public Bai_01() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
