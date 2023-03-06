d0d61bb842eaeba9ceecf033b08c43d67bacabc8
anhphanhumg <anhphanck@gmail.com>
2023-03-06 15:02:42 +0700

    Create Bai_02.java

---
+++ Bai_02.java
@@ -0,0 +1,26 @@
+package Bai_01;
+
+import java.util.Scanner;
+
+public class Bai_02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int a;
+		System.out.println("Nhap so nguyen a :");
+		a = sc.nextInt();
+		//kiem tra tinh chan le
+		if( a%2 == 0 ) {
+			System.out.println("a là số chẵn ");
+		}else {
+			System.out.println("a là số lẻ ");
+			
+			
+		}
+		
+	}
+
+	public Bai_02() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
