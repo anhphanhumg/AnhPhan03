cb01c11179559ac2c6a9a08b8ddd8b701863575f
anhphanhumg <anhphanck@gmail.com>
2023-03-06 14:47:20 +0700

    Update Bai_01.java

--- Bai_01.java
+++ Bai_01.java
@@ -1,8 +1,27 @@
 package Bai_01;
 
+import java.util.Scanner;
+
 public class Bai_01 {
 	public static void main(String[] args) {
-		System.out.println("Phan Tuan Anh hoc Java");
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		System.out.println("Nhap a = ");
+		a = sc.nextInt();
+		System.out.println("Nhap b = ");
+		b = sc.nextInt();
+		System.out.println("a + b = " +(a+b));
+		System.out.println("a - b = " +(a-b));
+		System.out.println("a * b = " +(a*b));
+		System.out.println("a / b = " +(a/b));
+		System.out.println("a % b = " +(a%b));
+		
+		System.out.println(a +" == " +b+":"+(a==b));
+		System.out.println(a +" != " +b+":"+(a!=b));
+		System.out.println(a +" < " +b+":"+(a<b));
+		System.out.println(a +" <= " +b+":"+(a<=b));
+		System.out.println(a +" > " +b+":"+(a>b));
+		System.out.println(a +" >= " +b+":"+(a>=b));
 	}
 
 	public Bai_01() {
