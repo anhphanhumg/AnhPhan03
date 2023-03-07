0c1d5c48f7159b7767b9b14cec4394c744ea1a42
anhphanhumg <anhphanck@gmail.com>
2023-03-07 18:43:28 +0700

    Create Bai_07.java

---
+++ Bai_07.java
@@ -0,0 +1,18 @@
+package Bai_01;
+
+import java.util.Iterator;
+
+public class Bai_07 {
+	public static void main(String[] args) {
+		for (int i = 1; i < 20; i++) {
+			System.out.println(i);
+			
+		}
+			
+		}
+
+	public Bai_07() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
