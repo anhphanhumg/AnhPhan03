41168f131a7bdc7c8417795aa7b3c99907d68b95
anhphanhumg <anhphanck@gmail.com>
2023-05-21 00:38:04 +0700

    Create test.java

---
+++ Bài 1 lab 4/test.java
@@ -0,0 +1,21 @@
+package Java_60;
+
+public class test {
+	public static void main(String[] args) {
+		Nguoi n = new Nguoi();
+		n.doTuoi();
+		n.tenNguoi();
+		n.gioiTinh();
+		n.loaiTienSu();
+		n.loaiChuanDoan();
+		n.tenBenhVien();
+		n.tenDiaChi();
+		n.tenGiamDoc();
+	}
+	
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
