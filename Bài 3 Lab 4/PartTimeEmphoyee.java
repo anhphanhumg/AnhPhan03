a12d1606d097da50e8d0c1400d3dcdca1a9eaff4
anhphanhumg <anhphanck@gmail.com>
2023-05-21 22:37:57 +0700

    Create PartTimeEmphoyee.java

---
+++ Bài 3 Lab 4/PartTimeEmphoyee.java
@@ -0,0 +1,33 @@
+package PartTimeEmployee;
+
+public class PartTimeEmphoyee {
+	int hoursPerWeek;
+	
+
+	public PartTimeEmphoyee(int hoursPerWeek) {
+		super();
+		this.hoursPerWeek = hoursPerWeek;
+	}
+	
+
+
+	public int getHoursPerWeek() {
+		return hoursPerWeek;
+	}
+
+
+
+	public void setHoursPerWeek(int hoursPerWeek) {
+		this.hoursPerWeek = hoursPerWeek;
+	}
+	public void hoursPerWeek() {
+		System.out.println("hoursPerWeek");
+	}
+
+
+
+	public PartTimeEmphoyee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
