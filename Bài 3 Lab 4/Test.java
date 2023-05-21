80f27a2a58e8b094ecb24bd61bb31c40c92413ee
anhphanhumg <anhphanck@gmail.com>
2023-05-21 22:38:01 +0700

    Create Test.java

---
+++ Bài 3 Lab 4/Test.java
@@ -0,0 +1,16 @@
+package PartTimeEmployee;
+
+public class Test {
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.employerName();
+		p.hoursPerWeek();
+		p.gender();
+		
+	}
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
