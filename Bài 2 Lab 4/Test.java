7448a218848724623ca2a6eee31df18e0bb23edd
anhphanhumg <anhphanck@gmail.com>
2023-05-21 22:19:39 +0700

    Create Test.java

---
+++ Bài 2 Lab 4/Test.java
@@ -0,0 +1,21 @@
+package SinhVien;
+
+public class Test {
+	public static void main(String[] args) {
+		Nguoi n1 = new Nguoi();
+		n1.tenDiaChi();
+		n1.tenHieuTruong();
+		n1.tenNguoi();
+		n1.tenTruong();
+		n1.gioiTinhNguoi();
+		n1.tuoiNguoi();
+	}
+	
+	
+	
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
